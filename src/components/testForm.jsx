d71364c08c58fa37c8082380ed45c1678e2bee5b
Ahmed Osama Hosney Mostafa <ahmed.osama@hotmail.co.uk>
2019-01-16 16:10:00 +0200

    Handle accessibility for the button

--- src/components/testForm.jsx
+++ src/components/testForm.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import "./testFormStyle.css";
+import "./buttonStyle.css";
 const TestForm = () => {
+  function actionButtonEventHandler(event) {
+    var type = event.type;
+    // Grab the keydown and click events
+    if (type === "keydown") {
+      // If either enter or space is pressed, execute the funtion
+      if (event.keyCode === 13 || event.keyCode === 32) {
+        alert("The button is working :D");
+        event.preventDefault();
+      }
+    } else if (type === "click") {
+      alert("The button is working :D");
+    }
+  }
+
   return (
     <div>
+      <br />
+      <div
+        tabindex="0"
+        role="button"
+        id="action"
+        onClick={actionButtonEventHandler}
+        onKeyDown={actionButtonEventHandler}
+      >
+        Print Page
+      </div>
+
       <br />
       <span>Hover the red box</span>
       <div id="movingDiv" />
